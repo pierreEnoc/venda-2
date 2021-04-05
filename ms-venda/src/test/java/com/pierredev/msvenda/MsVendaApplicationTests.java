c86a4435839dcbbadb1684bb5336ea37609045a6
Pierre enoc <pierre.enoc@gympass.com>
2021-04-05 17:00:57 -0300

    start project

---
+++ ms-venda/src/test/java/com/pierredev/msvenda/MsVendaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pierredev.msvenda;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsVendaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
